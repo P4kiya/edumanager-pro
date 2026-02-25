1481881c1ad2fc13dcf8405a2e79e7fc86a98b0e
P4kiya <pakiyaasiv1@gmail.com>
2026-02-25 13:20:41 +0000

    add backend spring boot

---
+++ backend/src/main/java/com/edumanager/api/EduManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.edumanager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduManagerApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/edumanager/api/EduManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edumanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
